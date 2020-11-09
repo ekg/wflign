7a0fa2ec1ac2033cb123758646686116036687f6
Erik Garrison <erik.garrison@gmail.com>
2020-11-09 20:07:13 +0100

    run with a kind of long downstream band

--- src/edlign.cpp
+++ src/edlign.cpp
@@ -8,14 +8,16 @@ void edlign(
         const std::string& target_name,
         const std::string& target,
         const uint64_t& segment_length) {
-    for (uint64_t i = 0; i < target.size(); i += segment_length) {
-        for (uint64_t j = 0; j < query.size(); j += segment_length) {
+    uint64_t step_size = segment_length / 4;
+    auto edlib_config = edlibNewAlignConfig(segment_length / 2, EDLIB_MODE_NW, EDLIB_TASK_DISTANCE, NULL, 0);
+    for (uint64_t i = 0; i < target.size(); i += step_size) {
+        for (uint64_t j = 0; j < query.size(); j += step_size) {
             uint64_t query_size = std::min(segment_length, query.size()-j);
-            uint64_t target_size = std::min(segment_length, target.size()-i);
+            uint64_t target_size = std::min(segment_length, target.size()-i);\
             EdlibAlignResult result = edlibAlign(query.c_str()+j, query_size,
                                                  target.c_str()+i, target_size,
-                                                 edlibDefaultAlignConfig());
-            if (result.status == EDLIB_STATUS_OK) {
+                                                 edlib_config);
+            if (result.status == EDLIB_STATUS_OK && result.editDistance >= 0) {
                 uint64_t max_size = std::max(query_size, target_size);
                 std::cout
                     << query_name << "\t" << target_name << "\t"
